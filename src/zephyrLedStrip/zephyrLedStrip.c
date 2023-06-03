e1a54f1c64d1eaf28c18c6f18733d33f4095179d
jbacon <julien.baccon.1@gmail.com>
2023-06-03 16:17:02 -0400

    clean up

--- src/zephyrLedStrip/zephyrLedStrip.c
+++ src/zephyrLedStrip/zephyrLedStrip.c
@@ -45,7 +45,7 @@ int zephyrLedStripInit(ZephyrLedStrip *strip, ZephyrLedStripClrFmt colorFmt,
         LOG_ERR("unable to allocate memory for %d X RGB pixel", pixelCnt);
         return -ENOSPC;
       }
-      memset(strip->rgbPixels, 0x00, sizeof(ZephyrRgbLed) * pixelCnt);
+      memset(strip->rgbPixels, 0x00, pixelCnt * sizeof(ZephyrRgbLed));
       break;
     default:
       LOG_ERR("color format %d is not supported", colorFmt);
