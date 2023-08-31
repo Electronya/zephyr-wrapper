f2832e280b2207dba733b177d74233b763f5c880
jbacon <julien.baccon.1@gmail.com>
2023-08-31 18:44:27 -0400

    adding a ring buffer wrapper

---
+++ src/zephyrRingBuffer/zephyrRingBuffer.c
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      zephyrRingBuffer.c
+ * @author    jbacon
+ * @date      2023-08-30
+ * @brief     Ring Buffer Wrapper
+ *
+ *            This file is the implementation of the ring buffer wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+#include "zephyrRingBuffer.h"
+#include "zephyrCommon.h"
+
+LOG_MODULE_DECLARE(ZEPHYR_WRAPPER_MODULE_NAME);
+
+void zephyrRingBufInit(ZephyrRingBuffer *buffer, size_t size, uint8_t *data)
+{
+  ring_buf_init(buffer);
+}
+
+bool zephyrRingBufIsEmpty(ZephyrRingBuffer *buffer)
+{
+  return ring_buf_is_empty(buffer);
+}
+
+void zephyrRingBufReset(ZephyrRingBuffer *buffer)
+{
+  ring_buf_reset(buffer);
+}
+
+size_t zephyrRingBufGetFreeSpace(ZephyrRingBuffer *buffer)
+{
+  return ring_buf_space_get(buffer);
+}
+
+size_t zephyrRingBufGetSize(ZephyrRingBuffer *buffer)
+{
+  return ring_buf_capacity_get(buffer);
+}
+
+size_t zephyrRingBufGetUsedSpace(ZephyrRingBuffer *buffer)
+{
+  return ring_buf_size_get(buffer);
+}
+
+size_t zephyrRingBufClaimForPutting(ZephyrRingBuffer *buffer, uint8_t **data,
+                                    size_t size)
+{
+  return ring_buf_put_claim(buffer, data, size);
+}
+
+int zephyrRingBufFinishPutting(ZephyrRingBuffer *buffer, size_t size)
+{
+  return ring_buf_put_finish(buffer, size);
+}
+
+size_t zephyrRingBufPut(ZephyrRingBuffer *buffer, uint8_t *data, size_t size)
+{
+  return ring_buf_put(buffer, data, size);
+}
+
+size_t zephyrRingBufClaimGetting(ZephyrRingBuffer *buffer, uint8_t **data,
+                                 size_t size)
+{
+  return ring_buf_get_claim(buffer, data, size);
+}
+
+int zephyrRingBufFinishingGetting(ZephyrRingBuffer *buffer, size_t size)
+{
+  return ring_buf_get_finish(buffer, size);
+}
+
+size_t zephyrRingBufGet(ZephyrRingBuffer *buffer, uint8_t *data, size_t size)
+{
+  return ring_buf_get(buffer, data, size);
+}
+
+size_t zephyrRingBufPeek(ZephyrRingBuffer *buffer, uint8_t *data, size_t size)
+{
+  return ring_buf_peek(buffer, data, size);
+}
+
+/** @} */
---
+++ src/zephyrRingBuffer/zephyrRingBuffer.h
@@ -0,0 +1,163 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      zephyrRingBuffer.h
+ * @author    jbacon
+ * @date      2023-08-30
+ * @brief     Ring Buffer Wrapper
+ *
+ *            This file is the declaration of the ring buffer wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#ifndef RING_BUFFER_WRAPPER
+#define RING_BUFFER_WRAPPER
+
+#include <zephyr/sys/ring_buffer.h>
+
+/**
+ * @brief The ring buffer type.
+*/
+typedef struct ring_buf ZephyrRingBuffer;
+
+/**
+ * @brief   Initialize a byte ring buffer.
+ *
+ * @param buffer  The ring buffer structure.
+ * @param size    The size of the ring buffer in bytes.
+ * @param data    The data area of the ring buffer.
+ */
+void zephyrRingBufInit(ZephyrRingBuffer *buffer, size_t size, uint8_t *data);
+
+/**
+ * @brief   Check if a ring buffer is empty.
+ *
+ * @param buffer  The ring buffer.
+ *
+ * @return  true if the ring buffer is empty, false otherwise.
+ */
+bool zephyrRingBufIsEmpty(ZephyrRingBuffer *buffer);
+
+/**
+ * @brief   Reset a ring buffer.
+ *
+ * @param buffer  The ring buffer.
+ */
+void zephyrRingBufReset(ZephyrRingBuffer *buffer);
+
+/**
+ * @brief   Get the free space of a byte ring buffer.
+ *
+ * @param buffer  The ring buffer.
+ *
+ * @return  The free space in bytes.
+ */
+size_t zephyrRingBufGetFreeSpace(ZephyrRingBuffer *buffer);
+
+/**
+ * @brief   Get a rig buffer total size.
+ *
+ * @param buffer  The ring buffer.
+ *
+ * @return  The size in bytes.
+ */
+size_t zephyrRingBufGetSize(ZephyrRingBuffer *buffer);
+
+/**
+ * @brief   Get a ring buffer used space.
+ *
+ * @param buffer  The ring buffer.
+ *
+ * @return  The size in bytes.
+ */
+size_t zephyrRingBufGetUsedSpace(ZephyrRingBuffer *buffer);
+
+/**
+ * @brief   Claim a ring buffer for putting.
+ *
+ * @param buffer  The ring buffer.
+ * @param data    The pointer to the data area claimed.
+ * @param size    The size of the data area to claim.
+ *
+ * @return  The size of the claimed area, which may be smaller than the
+ *          requested one.
+ */
+size_t zephyrRingBufClaimForPutting(ZephyrRingBuffer *buffer, uint8_t **data,
+                                    size_t size);
+
+/**
+ * @brief   Signal that the putting operation is done. This needs to be called
+ *          after a claim putting operation.
+ *
+ * @param buffer  The ring buffer.
+ * @param size    The size of the data that was put in the ring buffer.
+ *
+ * @return  0 if successful, the error code otherwise.
+ */
+int zephyrRingBufFinishPutting(ZephyrRingBuffer *buffer, size_t size);
+
+/**
+ * @brief   Put data in a ring buffer.
+ *
+ * @param buffer  The ring buffer.
+ * @param data    The data.
+ * @param size    The size of the data.
+ *
+ * @return  The size of the data that was really put in the buffer. Which can
+ *          be smaller than the size of the data.
+ */
+size_t zephyrRingBufPut(ZephyrRingBuffer *buffer, uint8_t *data, size_t size);
+
+/**
+ * @brief   Claim a ring buffer for getting data.
+ *
+ * @param buffer  The ring buffer.
+ * @param data    The pointer to the data area claimed.
+ * @param size    The size of the data area to claim.
+ *
+ * @return  The actual size of the data area claimed. Which may differ from
+ *          the size requested.
+ */
+size_t zephyrRingBufClaimGetting(ZephyrRingBuffer *buffer, uint8_t **data,
+                                 size_t size);
+
+/**
+ * @brief   Signal that the getting operation is done. This needs to be called
+ *          after the claim getting operation.
+ *
+ * @param buffer
+ * @param size
+ * @return int
+ */
+int zephyrRingBufFinishingGetting(ZephyrRingBuffer *buffer, size_t size);
+
+/**
+ * @brief   Get data from a ring buffer.
+ *
+ * @param buffer  The ring buffer.
+ * @param data    The output buffer.
+ * @param size    The size of the output buffer.
+ *
+ * @return  The actual data that was get from the ring buffer. Which may differ
+ *          from the requested size.
+ */
+size_t zephyrRingBufGet(ZephyrRingBuffer *buffer, uint8_t *data, size_t size);
+
+/**
+ * @brief   Peek into a ring buffer. Peeking allows to read data from a ring
+ *          buffer without removing the data from it.
+ *
+ * @param buffer  The ring buffer.
+ * @param data    The output buffer.
+ * @param size    The size of the output buffer.
+ *
+ * @return  The size of the data peeked from the buffer. Which can differ from
+ *          the requested size.
+ */
+size_t zephyrRingBufPeek(ZephyrRingBuffer *buffer, uint8_t *data, size_t size);
+
+#endif    /* RING_BUFFER_WRAPPER */
+
+/** @} */
