9b879aaaf2bb044e4ba5029c9ddd30a51b44bb15
jbacon <julien.baccon.1@gmail.com>
2023-09-22 05:56:22 -0400

    first drat done

---
+++ src/zephyrAdc/zephyrAdc.c
@@ -0,0 +1,117 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      zephyrAdc.c
+ * @author    jbacon
+ * @date      2023-09-21
+ * @brief     ADC Wrapper
+ *
+ *            This file is the implementation of the ADC wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+#include "zephyrAdc.h"
+#include "zephyrCommon.h"
+
+LOG_MODULE_REGISTER(ZEPHYR_WRAPPER_MODULE_NAME);
+
+typedef struct adcCtrlData
+{
+  const struct device *adc;           /**< The zephyr ADC device. */
+  struct adc_channel_cfg *chanCfgs;   /**< The zephyr ADC channel configuration. */
+  uint32_t *buffers;                  /**< The ADC buffers. */
+  uint8_t chanUsedCnt;                /**< The number of ADC channel used. */
+  struct adc_sequence seq;            /**< The zephyr ADC sequence. */
+  uint32_t vdd;                       /**< The VDD value in mV. */
+} AdcCtrlData;
+
+/**
+ * @brief The ADC control data.
+*/
+static AdcCtrlData adcCtrlData;
+
+/**
+ * @brief   Initialize an ADC channel.
+ *
+ * @param config  The ADC channel configuration.
+ *
+ * @return  0 if successful, the error code otherwise.
+ */
+int initAdcChannel(ZephyrAdcChanConfig *config)
+{
+  int rc;
+
+  adcCtrlData.chanCfgs[config->id].channel_id = config->id;
+  adcCtrlData.chanCfgs[config->id].gain = config->gain;
+  adcCtrlData.chanCfgs[config->id].acquisition_time =
+    ADC_ACQ_TIME(ADC_ACQ_TIME_TICKS, config->acqTime);
+  adcCtrlData.chanCfgs[config->id].reference = config->ref;
+
+  rc = adc_channel_setup(adcCtrlData.adc, adcCtrlData.chanCfgs + config->id);
+  if(rc < 0)
+    LOG_ERR("unable to setup channel %d: %d", config->id, rc);
+
+  return rc;
+}
+
+int zephyrAdcInit(ZephyrAdcChanConfig *configs, size_t chanCount,
+                  ZephyrAdcRes res, uint32_t vdd)
+{
+  int rc = 0;
+
+  adcCtrlData.adc = DEVICE_DT_GET(DT_ALIAS(adc));
+  if(!adcCtrlData.adc)
+  {
+    LOG_ERR("unable to get ADC device");
+    return -ENODEV;
+  }
+
+  adcCtrlData.vdd = vdd;
+  adcCtrlData.seq.resolution = res;
+  adcCtrlData.chanUsedCnt = chanCount;
+  adcCtrlData.chanCfgs = k_malloc(sizeof(ZephyrAdcChanConfig) * chanCount);
+  if(!adcCtrlData.chanCfgs)
+  {
+    LOG_ERR("unable to allocate all the ADC channel config");
+    return -ENOSPC;
+  }
+
+  for(uint32_t i = 0; i < chanCount && rc == 0; ++i)
+  {
+    rc = initAdcChannel(configs + i);
+  }
+
+  if(rc < 0)
+    k_free(adcCtrlData.chanCfgs);
+
+  return rc;
+}
+
+int zephyrAdcGetSample(ZephyrAdcChanId id, uint32_t *sample)
+{
+  int rc;
+
+  adcCtrlData.seq.channels = BIT(id);
+  adcCtrlData.seq.buffer = (void *)sample;
+
+  rc = adc_read(adcCtrlData.adc, adcCtrlData.seq);
+  if(rc < 0)
+  {
+    LOG_ERR("unable to sample ADC channel %d: %d", id, rc);
+    return rc;
+  }
+
+  rc = adc_raw_to_millivolts(adcCtrlData.vdd, adcCtrlData.chanCfgs[id].gain,
+    adcCtrlData.seq.resolution, sample);
+  if(rc < 0)
+    LOG_ERR("unable to convert ADC raw data to mV of channel %d: %d", id, rc);
+
+  return rc;
+}
+
+/** @} */
---
+++ src/zephyrAdc/zephyrAdc.h
@@ -0,0 +1,130 @@
+/**
+ * Copyright (C) 2023 by Electronya
+ *
+ * @file      zephyrAdc.h
+ * @author    jbacon
+ * @date      2023-09-21
+ * @brief     ADC Wrapper
+ *
+ *            This file is the declaration of the ADC wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#ifndef ZEPHYR_ADC_WRAPPER
+#define ZEPHYR_ADC_WRAPPER
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/adc.h>
+
+/**
+ * @brief ADC channel ID. These channel are based on the STM32 devices.
+ */
+typedef enum
+{
+  ADC_CHAN_0 = 0,                   /**< The ADC channel 0. */
+  ADC_CHAN_1,                       /**< The ADC channel 1. */
+  ADC_CHAN_2,                       /**< The ADC channel 2. */
+  ADC_CHAN_3,                       /**< The ADC channel 3. */
+  ADC_CHAN_4,                       /**< The ADC channel 4. */
+  ADC_CHAN_5,                       /**< The ADC channel 5. */
+  ADC_CHAN_6,                       /**< The ADC channel 6. */
+  ADC_CHAN_7,                       /**< The ADC channel 7. */
+  ADC_CHAN_8,                       /**< The ADC channel 8. */
+  ADC_CHAN_9,                       /**< The ADC channel 9. */
+  ADC_CHAN_10,                      /**< The ADC channel 10. */
+  ADC_CHAN_11,                      /**< The ADC channel 11. */
+  ADC_CHAN_12,                      /**< The ADC channel 12. */
+  ADC_CHAN_13,                      /**< The ADC channel 13. */
+  ADC_CHAN_14,                      /**< The ADC channel 14. */
+  ADC_CHAN_15,                      /**< The ADC channel 15. */
+  ADC_DIE_TEMP,                     /**< The die temperature ADC channel. */
+  ADC_VREF,                         /**< The Vref ADC channel*/
+  ADC_MAX_CHAN_CNT,                 /**< The ADC maximal channel count. */
+} ZephyrAdcChanId;
+#endif    /* ZEPHYR_ADC_WRAPPER */
+
+/**
+ * @brief ADC channel gains.
+ */
+typedef enum
+{
+  ADC_UNIT_GAIN = ADC_GAIN_1,       /**< The ADC 0 gain level. */
+} ZephyrAdcChanGain;
+
+/**
+ * @brief ADC channel reference.
+ */
+typedef enum
+{
+  ADC_REF_VDD = ADC_REF_VDD_1,            /**< VDD. */
+	ACD_REF_VDD_1_2 = ADC_REF_VDD_1_2,      /**< VDD/2. */
+	ADC_REF_VDD_1_3 = ADC_REF_VDD_1_3,      /**< VDD/3. */
+	ADC_REF_VDD_1_4 = ADC_REF_VDD_1_4,      /**< VDD/4. */
+  ADC_REF_INT = ADC_REF_INTERNAL,         /**< The ADC channel internal reference. */
+  ADC_REF_EXT0 = ADC_REF_EXTERNAL0,       /**< External, input 0. */
+	ADC_REF_EXT0 = ADC_REF_EXTERNAL1,       /**< External, input 1. */
+} ZephyrAdcChanRef;
+
+/**
+ * @brief ADC channel acquisition time.
+ */
+typedef enum
+{
+  ADC_ACQ_2_CYCLES = 2,             /**< The ADC channel 2 cycles acquisition time. */
+  ADC_ACQ_8_CYCLES = 8,             /**< The ADC channel 8 cycles acquisition time. */
+  ADC_ACQ_14_CYCLES = 14,           /**< The ADC channel 14 cycles acquisition time. */
+  ADC_ACQ_29_CYCLES = 29,           /**< The ADC channel 29 cycles acquisition time. */
+  ADC_ACQ_42_CYCLES = 42,           /**< The ADC channel 42 cycles acquisition time. */
+  ADC_ACQ_56_CYCLES = 56,           /**< The ADC channel 56 cycles acquisition time. */
+  ADC_ACQ_72_CYCLES = 72,           /**< The ADC channel 72 cycles acquisition time. */
+  ADC_ACQ_240_CYCLES = 240,         /**< The ADC channel 240 cycles acquisition time. */
+} ZephyrAdcChanAcqTime;
+
+/**
+ * @brief ADC resolution.
+ */
+typedef enum
+{
+  ADC_6BITS_RES = 6,                /**< The ADC 6 bits resolution. */
+  ADC_8BITS_RES = 8,                /**< The ADC 8 bits resolution. */
+  ADC_10BITS_RES = 10,              /**< The ADC 10 bits resolution. */
+  ADC_12BITS_RES = 12,              /**< The ADC 12 bits resolution. */
+} ZephyrAdcRes;
+
+/**
+ * @brief ADC channel configuration.
+ */
+typedef struct adcChanConfig
+{
+  ZephyrAdcChanId id;               /**< The ADC channel ID. */
+  ZephyrAdcChanGain gain;           /**< The ADC channel gain. */
+  ZephyrAdcChanRef ref;             /**< The ADC channel reference. */
+  ZephyrAdcChanAcqTime acqTime;     /**< The ADC channel acquisition time. */
+} ZephyrAdcChanConfig;
+
+/**
+ * @brief   Initialize the ADC.
+ *
+ * @param configs     The ADC channel configuration.
+ * @param chanCount   The number of ADC channel configuration.
+ * @param res         The ADC resolution.
+ * @param vdd         The VDD reference voltage in mV.
+ *
+ * @return  0 if successful, the error code otherwise.
+ */
+int zephyrAdcInit(ZephyrAdcChanConfig *configs, size_t chanCount,
+                  ZephyrAdcRes res, uint32_t vdd);
+
+/**
+ * @brief   Get an ADC sample from the specified channel.
+ *
+ * @param id          The ADC channel ID.
+ * @param sample      The sample.
+ *
+ * @return  0 fi successful, the error code otherwise.
+ */
+int zephyrAdcGetSample(ZephyrAdcChanId id, uint32_t *sample);
+
+/** @} */
