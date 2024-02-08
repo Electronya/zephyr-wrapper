8b5389f18bcfad2dfc8a350664d7b643101d8f2e
jbacon <julien.bacon.1@gmail.com>
2024-02-08 17:21:38 -0500

    adding the nvs settings

---
+++ src/zephyrSettings/zephyrNvsSettings.c
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C) 2024 by Electronya
+ *
+ * @file      zephyrNvsSettings.h
+ * @author    jbacon
+ * @date      2024-02-08
+ * @brief     NVS Settings Wrapper
+ *
+ *            This file is the implementation of the NVS setting wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#include "zephyrNvsSettings.h"
+#include "zephyrCommon.h"
+
+LOG_MODULE_DECLARE(ZEPHYR_WRAPPER_MODULE_NAME);
+
+/** @} */
---
+++ src/zephyrSettings/zephyrNvsSettings.h
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) 2024 by Electronya
+ *
+ * @file      zephyrNvsSettings.h
+ * @author    jbacon
+ * @date      2024-02-08
+ * @brief     NVS Settings Wrapper
+ *
+ *            This file is the declaration of the NVS settings wrapper.
+ *
+ * @ingroup  zephyr-wrapper
+ * @{
+ */
+
+#ifndef NVS_SETTINGS_WRAPPER
+#define NVS_SETTINGS_WRAPPER
+
+#include <zephyr/kernel.h>
+
+#endif    /* NVS_SETTINGS_WRAPPER */
+
+/** @} */
